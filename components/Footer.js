34aa133c6a74dd59fec0c6a444d1c6886f1cd257
Sanna Mäkinen <sanna.makinen@gmail.com>
2023-01-19 21:36:40 +0200

    Replace Twitter profile link with a link to my accessibility site

--- components/Footer.js
+++ components/Footer.js
@@ -35,21 +35,32 @@ const Footer = () => {
         </a>
         <div className="links">
           <a
-            href="https://twitter.com/schalallalaa"
+            href="https://a11y.sanna.ninja"
             className="flex align-center text-lt-blue-dark lg:text-xl font-bold border-y-4 border-transparent p-2 m-1 text-lt-blue-darkest dark:text-dk-blue-light
             hover:text-lt-purple hover:dark:text-dk-blue-light hover:border-y-4 hover:border-lt-purple dark:hover:border-dk-blue-light
             focus:outline focus:outline-2 focus:outline-offset-8	focus:outline-black dark:focus:outline-white"
             target="_blank"
             rel="noopener noreferrer"
           >
+          <span className="logo-dark mr-2.5">
             <Image
-              src="/icons/twitter.svg"
+              src="/sm-logo-darkblue.png"
               alt=""
-              width={30}
-              height={30}
+              width={35}
+              height={27}
               aria-hidden="true"
             />
-            <span className="ml-2.5">My Twitter profile</span>
+          </span>
+          <span className="logo-light mr-2.5">
+            <Image
+              src="/sm-logo-lightblue.png"
+              alt=""
+              width={35}
+              height={27}
+              aria-hidden="true"
+            />
+          </span>
+            <span className="ml-2.5">My accessibility site</span>
           </a>
         </div>
       </div>
